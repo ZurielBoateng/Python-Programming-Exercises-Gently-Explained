c195a561aa5c1e697da929b5025d32cbd32e1e90
Z <105754053+ZurielBoateng@users.noreply.github.com>
2023-12-06 15:13:59 +0000

    Add files via upload

---
+++ EXERCISE 1.py
@@ -0,0 +1,7 @@
+# Write a program that, when run, greets the user by printing ―Hello, world!‖ on the screen. 
+# Then it prints a message on the screen asking the user to enter their name. 
+# The program greets the user by name by printing the ―Hello,‖ followed by the user’s name.
+
+print ("Hello, World!")
+user_name = input ("What is your name? ")
+print ("Hello, "+ user_name)
